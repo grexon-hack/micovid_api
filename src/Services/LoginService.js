0dae63d5e9db31261661cdc5858a51c2022a6d24
WilmerMaza <wilmermaza10@gmail.com>
2023-07-11 21:30:03 -0500

    Cambios de encriptacion

--- src/Services/LoginService.js
+++ src/Services/LoginService.js
@@ -3,8 +3,8 @@ const { Tenants, TenantSettings, jwt } = require('../db.js');
 
 const Login = async (Name, Password) => {
     const CryptoJS = require('crypto-js');
-    const secretKey = '1234567890';
-    const pass = CryptoJS.AES.decrypt(Password, secretKey).toString(CryptoJS.enc.Utf8);
+    const { SECRETKEY } = process.env
+    const pass = CryptoJS.AES.decrypt(Password, SECRETKEY).toString(CryptoJS.enc.Utf8);
     try {
         const dataBd = await Tenants.findOne({
             where: {
